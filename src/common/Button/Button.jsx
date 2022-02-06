02ca82d8e1dce30229ca010d2a1216690be5db0e
Vladislav Cheliadin <Vladislav_Cheliadin@epam.com>
2022-02-06 14:20:21 +0300

    u

--- src/common/Button/Button.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Button as Btn } from 'semantic-ui-react';
-
-export default function Button(props) {
-	return <Btn basic color='violet' {...props} />;
-}
--- src/components/Header/components/Logo/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Logo() {
-	return <img alt='logo' src='/logo192.png' />;
-}
